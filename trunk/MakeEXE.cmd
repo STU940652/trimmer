0b81fa075a2ee2201d1d7028e9d14e03e67fa4bc
andyndeanna@gmail.com <andyndeanna@gmail.com@f1c749e3-e1c7-4f39-a8f1-d546e7bbaac3>
2014-12-27 03:22:41 +0000

    Updated for pyInstaller 2.1

--- trunk/MakeEXE.cmd
+++ trunk/MakeEXE.cmd
@@ -1,3 +1,4 @@
 @echo off
 rem pyinstaller-2.0\pyinstaller.py -w Trimmer.py
-C:\python27\python.exe pyinstaller-2.0\pyinstaller.py Trimmer.spec
+rem C:\python27\python.exe pyinstaller-2.0\pyinstaller.py Trimmer.spec
+pyinstaller Trimmer.spec
