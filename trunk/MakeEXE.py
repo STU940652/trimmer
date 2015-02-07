396bbccddce20053754406b3de36273dc7c2191a
andyndeanna@gmail.com <andyndeanna@gmail.com@f1c749e3-e1c7-4f39-a8f1-d546e7bbaac3>
2015-02-07 18:51:11 +0000

    Updated FMPEG. Added licenses. Rebuilt.

--- trunk/MakeEXE.py
+++ trunk/MakeEXE.py
@@ -4,7 +4,12 @@ from cx_Freeze import setup, Executable
 # fine tuning.
 buildOptions = dict(packages = [], 
                     excludes = [], 
-                    include_files = ['ffmpeg.exe', 'Trimmer.ini', 'Format for EW.cmd']
+                    include_files = [   'ffmpeg.exe', 
+                                        'Trimmer.ini', 
+                                        'LICENSES_FFMPEG.zip',
+                                        'lgpl-2.1.txt',
+                                        'ReadMe.txt',
+                                        'Format for EW.cmd']
                     )
 
 import sys
