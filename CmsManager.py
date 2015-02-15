ed1f48bcd86c9b74f2261a1b1c7b3cb3643c3fc0
Andy Martin <25186508+STU940652@users.noreply.github.com>
2015-02-15 17:03:24 +0000

    Added manager to get sermon info from ekklesia360 CMS.

---
+++ CmsManager.py
@@ -0,0 +1,78 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from credentials import *
+
+class CmsManager ():
+    def __init__ (self):
+        self.IsLoggedIn = False
+        self.EventInfo = {}
+        self.driver = None
+
+    def Login(self):
+        if not self.IsLoggedIn:
+            if self.driver == None:
+                self.driver = webdriver.Firefox()
+            self.driver.get("http://my.ekklesia360.com/Login")
+            self.driver.switch_to_frame('_monkIdXdm')
+            b = self.driver.find_element_by_id('button')
+            b.click()
+            time.sleep(5)
+            self.driver.switch_to_window(self.driver.window_handles[1])
+            b = self.driver.find_element_by_id('user_email')
+            b.send_keys(cms_username)
+            b = self.driver.find_element_by_id('user_password')
+            b.send_keys(cms_password)
+            b = self.driver.find_element_by_name('button')
+            b.click()
+            time.sleep(5)
+
+            # We are now logged in
+            self.driver.switch_to_window(self.driver.window_handles[0])
+            
+            # We have succeeded to log in
+            return True
+            
+    def GetEventInfo (self):
+        self.EventInfo = {}
+        if not self.IsLoggedIn:
+            if not self.Login():
+                return self.EventInfo
+                
+        self.driver.get("https://my.ekklesia360.com/Sermon/list")
+
+        l=self.driver.find_element_by_id('listOutput')
+        l.find_elements_by_class_name ('title')[1].find_elements_by_tag_name('a')[0].click()
+        time.sleep(5)
+
+        self.EventInfo["title"] = self.driver.find_element_by_id("title").get_attribute("value")
+
+        l = self.driver.find_element_by_id("series_list")
+        for i in l.find_elements_by_tag_name('option'):
+            if i.get_attribute("selected"):
+                self.EventInfo["series"] = i.text
+           
+        self.EventInfo["date"] = self.driver.find_element_by_id("sermondate").get_attribute("value")
+
+        self.EventInfo["speaker"] = self.driver.find_element_by_id("selection").find_elements_by_tag_name('span')[0].text
+        
+        return self.EventInfo
+
+    def Close( self):
+        self.__exit__ (None, None, None)
+    
+    def __enter__(self):
+        return self
+        
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.driver != None:
+            self.driver.close()
+            
+        self.IsLoggedIn = False
+        self.EventInfo = {}
+        self.driver = None
+        
+        return False
+
+            
+        
