1a29d13f7081300776c6e2103fac1d3e41743192
Andy Martin <25186508+STU940652@users.noreply.github.com>
2015-03-06 02:43:20 +0000

    Working on cross-platform copy.

--- Trimmer.py
+++ Trimmer.py
@@ -121,10 +121,12 @@ class TrimmerMain (wx.Frame):
         self.playerpanel.OpenFile(DestFilename, Play = False)
         
     def CloseEvent (self, evt): 
+        print ("Closing")
         self.cancelQueue.put("exit")
         #m = wx.MessageDialog(self, "Waiting for encoding to complete.  See other window for progress.",  "Please Wait...")
         #m.Show()
         self.EncodeThread.join()
+        print ("Done")
         self.Destroy()
 
     def OnViewLogDir(self, evt):
