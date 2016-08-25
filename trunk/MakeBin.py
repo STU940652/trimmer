550722334961b84711dc5ce7bfa75df889568bc8
stu940652 <stu940652@f1c749e3-e1c7-4f39-a8f1-d546e7bbaac3>
2016-08-25 20:07:57 +0000

    Working on installer.

--- trunk/MakeBin.py
+++ trunk/MakeBin.py
@@ -1,4 +1,6 @@
 from cx_Freeze import setup, Executable
+import subprocess
+import re
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
@@ -19,8 +21,20 @@ executables = [
     Executable('Trimmer.py', base=base)
 ]
 
+# Build version string
+version = '1.0'
+
+if sys.platform=='win32':
+    svn_info = subprocess.check_output('svn info | find "Last Changed Rev:"', shell=True).decode('utf-8')
+    svn_match = re.match("Last Changed Rev: *([0-9]+)", svn_info)
+    if svn_match:
+        version += '.'+svn_match.group(1)
+    
+    with open ("version.iss", "wt") as f:
+        f.write('#define MyAppVersion "%s"\n' % version)
+
 setup(name='trimmer',
-      version = '1.0',
+      version = version,
       description = 'Video File Trimmer',
       options = dict(build_exe = buildOptions),
       executables = executables)
